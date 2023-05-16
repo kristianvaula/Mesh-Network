ce0b438648424e31a4286aa8f4891e60b2ad4e06
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-05-16 14:42:44 +0200

    edit: server to be a class

--- service/Server.cpp
+++ service/Server.cpp
@@ -1,3 +1,6 @@
+//
+// Created by HansMagne Asheim on 16/05/2023.
+//
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,95 +12,126 @@
 #include <thread>
 #include <unordered_map>
 
-#include "../model/dto/SocketData.cpp"
-#include "../model/enums/Actiontype.cpp"
-#include "../model/DroneList.cpp"
+#include "../model/enums/ActionType.cpp"
+#include "../model/NodeList.h"
 
 #define PORT 1026
 
-int main(int argc, char const* argv[])
-{
-    std::unordered_map<std::string, ActionType> actionTypes = {
-            { "Low battery", ActionType::LOW_BATTERY },
-            { "Add drone", ActionType::ADD_DRONE }
-    };
-
-    DroneList droneList;
-
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+class Server {
+private:
+    std::unordered_map<std::string, ActionType> actionTypes;
     std::vector<std::thread> threads;
+    NodeList nodeList;
+    int server_fd;
+    bool run;
 
+public:
+    Server()
+        : actionTypes(),
+          threads(),
+          nodeList(),
+          server_fd(-1),
+          run(true) {}
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    void start() {
+        createSocket();
+        bindSocket();
+        listenForConnections();
+        handleConnections();
+        cleanup();
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    void stop() {
+        run = false;
+        shutdown(server_fd, SHUT_RDWR);
     }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    bool run = true;
-    while (run) {
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("new connection failed");
+private:
+    void createSocket() {
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            perror("socket failed");
             exit(EXIT_FAILURE);
         }
+    }
 
-        threads.emplace_back([&new_socket, &droneList, &actionTypes] {
-            int valread;
-            char buffer[1024] = { 0 };
-            std::string hello = "Hello from server";
+    void bindSocket() {
+        struct sockaddr_in address;
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(PORT);
 
-
-            try {
-                send(new_socket, hello.c_str(), hello.length(), 0);
-                valread = read(new_socket, buffer, 1024);
-                printf("%s\n", buffer);
-
-                DroneData droneData = {0};
-                long client_data = recv(new_socket, &droneData, sizeof(droneData), 0);
-                std::cout << "droneId: " << droneData.droneId << std::endl << "port: " << droneData.port << std::endl << "action: " << droneData.action << std::endl;
-                if (actionTypes[droneData.action] == ActionType::ADD_DRONE) {
-                    droneList.addDrone(droneData);
-                }
-
-                std::string client_ans = "The data is received from the server";
-                send(new_socket, client_ans.c_str(), client_ans.length(), 0);
-
-                close(new_socket);
-            } catch (...) {
-                close(new_socket);
-            }
-        });
-
-        char x;
-        std::cout << "Do you want the server to terminate (Y/N)" << std::endl;
-        std::cin >> x;
-        if (x == 'Y') {
-            run = false;
-            shutdown(server_fd, SHUT_RDWR);
+        // Forcefully attaching socket to the port
+        if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
         }
     }
 
-    for (auto& thread : threads) {
-        thread.join();
+    void listenForConnections() {
+        if (listen(server_fd, 3) < 0) {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
     }
+
+    void handleConnection(int new_socket) {
+        int valread;
+        char buffer[1024] = { 0 };
+        std::string hello = "Hello from server";
+
+        try {
+            send(new_socket, hello.c_str(), hello.length(), 0);
+            valread = read(new_socket, buffer, 1024);
+            printf("%s\n", buffer);
+
+            NodeData nodeData = {0};
+            long client_data = recv(new_socket, &nodeData, sizeof(nodeData), 0);
+            std::cout << "droneId: " << nodeData.nodeId << std::endl
+            << "port: " << nodeData.port << std::endl
+            << "action: " << nodeData.action << std::endl;
+            if (actionTypes[nodeData.action] == ActionType::ADD_NODE) {
+                nodeList.addNode(nodeData);
+            }
+
+            std::string client_ans = "The data is received from the server";
+            send(new_socket, client_ans.c_str(), client_ans.length(), 0);
+
+            close(new_socket);
+        } catch (...) {
+            close(new_socket);
+        }
+    }
+
+    void handleConnections() {
+        struct sockaddr_in address;
+        int addrlen = sizeof(address);
+
+        while (run) {
+            int new_socket;
+            if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+                perror("new connection failed");
+                exit(EXIT_FAILURE);
+            }
+
+            threads.emplace_back([&] {
+                handleConnection(new_socket);
+            });
+
+        }
+    }
+
+    void cleanup() {
+        for (auto& thread : threads) {
+            thread.join();
+        }
+    }
+};
+
+int main(int argc, char const* argv[])
+{
+    Server server;
+    server.start();
+
     return 0;
 }
-
